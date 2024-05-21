1f783c5f6fe41c2b2e8091c25a261311235bef54
Lith3 <jorisbudin@gmail.com>
2024-05-21 11:57:54 +0200

    finish quest 03 - Harmonia : Routing

---
+++ server/app/routers/api/programs/router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import item-related actions
+const { browse} = require("../../../controllers/programActions");
+
+// Route to get a list of items
+router.get("/", browse);
+
+
+
+/* ************************************************************************* */
+
+module.exports = router;
